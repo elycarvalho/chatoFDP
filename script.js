983ea5aba4d76e031eb6a60dcd52935dc7da9a88
Ely Carvalho <elycarvalho3@gmail.com>
2023-05-15 14:09:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let digitado = ''
 texto.focus()
 
 function send() {
+	document.querySelector('.aviso').style.display = 'none'
 	chat.innerHTML += `
 	    <div class="typed-text">
 	        <img src="./img/user.png" width="30">
@@ -45,13 +46,15 @@ function geraResposta(){
         mostraResposta("Pronto, já testou!")
 	}else if(digitado === "filho da puta"){
 		mostraResposta("você que é!")
+	}else if(trecho === "sera" || digitado === "será"){
+        mostraResposta("será que existe vida fora da terra? será que o sabiá sabia assobiar? será que você é o unico que me faz pergunta idiota?")
 	}else if(digitado === "viado"){
-        mostraResposta("'veado' é um animal mamífero ruminante da familia cervidae. A palavra 'viado' não existe.")
+        mostraResposta("'veado' é um animal mamífero ruminante da familia cervidae. A palavra 'viado' não existe seu burro.")
 	}else if(digitado === "vai se fuder" || digitado === "vai tomar no cu" || digitado === "fodas" || digitado === "vai pra puta que pariu"){
 		mostraResposta("Vai você seu mal-educado!")
 	}else if(digitado === "o que você faz?" || digitado === "você faz o que?"){
         mostraResposta("Respondo perguntas, mesmo as idiotas como essa!")
-	}else if(digitado === "você vai dominar o mundo?"){
+	}else if(digitado === "você vai dominar o mundo?" || digitado === "as IA's vao dominar o mundo?"){
 		mostraResposta("&#128517; hahahahaha viajou legal agora! Está assistindo muita ficção científica.")
 	}else if(trecho === "qual"){
         mostraResposta("não sei qual é, não sei qual foi, nem quero saber!")
@@ -65,6 +68,10 @@ function geraResposta(){
 		mostraResposta("cara, eu não sei nem onde eu estou, nem aonde vou, nem onde judas perdeu as botas...")
 	}else if(trecho === "fala" || trecho === "fale" || trecho === "diga"){
 		mostraResposta("não vou dizer nada, porque não sei falar, só sei escrever!")
+	}else if(trecho === "what" || trecho === "why" || trecho === "who" || trecho === "wher" || trecho === "how "){
+        mostraResposta("Eu não falo nem português direito, muito menos inglês!")
+	}else if(trecho === "exis"){
+        mostraResposta("existe papai noel, existe coelhinho da páscoa, fada do dente, etc. Só não existe chance de você ficar inteligente!")
 	}else if(trecho === "por " || trecho === "porq"){
         mostraResposta("são tantos 'por ques' nesse mundo... a verdade é que eu não sei o por que!")
 	}else{
@@ -78,7 +85,10 @@ function geraResposta(){
             "não deu pra entender! Tenta de novo, mas escreve direito dessa vez!",
             "do jeito que você escreveu, só um telepata pra ler sua mente e entender!",
             "que diacho é isso que você escreveu? Não entendi nada!",
-            "Não entendi. Você escreve tão mal que nem o google vai entender!"
+            "Não entendi. Você escreve tão mal que nem o google vai entender!",
+            "se você continuar escrevendo mal assim, não vai ter como a gente conversar!",
+            "por favor, tente escrever de um jeito que eu entenda, seu analfabeto!",
+            "isso que você escreveu parace árabe, grego ou russo, sei lá! Escreva em português e de forma clara"
 		]
 		let respAleatoria = Math.trunc(Math.random() * resps.length)
 		mostraResposta(

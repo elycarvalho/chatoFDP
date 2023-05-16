37c0a6f6689a3c8b11bf76686be91e7be1fc21d4
Ely Carvalho <elycarvalho3@gmail.com>
2023-05-16 05:26:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const texto = document.querySelector('.texto')
 const chat = document.querySelector('.chat')
 const menu = document.querySelector('.menu')
+const aviso = document.querySelector('.aviso')
 let digitado = ''
+const perguntas = []
 
 texto.focus()
 
 function send() {
-	document.querySelector('.aviso').style.display = 'none'
+	perguntas.push(texto.value)
+	console.log(perguntas)
+	aviso.style.display = 'none'
 	chat.innerHTML += `
 	    <div class="typed-text">
 	        <img src="./img/user.png" width="30">
@@ -28,26 +32,47 @@ function limpa(){
 }
 
 function mostraMenu() {
+	menu.innerHTML = ''
 	if(menu.style.display === 'flex'){
 		menu.style.display = 'none'
 	}else{
 		menu.style.display = 'flex'
 	}
+	for (let i = 0; i < perguntas.length; i++) {
+		menu.innerHTML += `
+            <p class="item-lista" id="${i}" onclick="escolheItem(this)">${perguntas[i]}</p>
+		`
+	}
+}
+
+function escolheItem(e){
+	texto.value = perguntas[e.id]
+	console.log(perguntas[e.id])
+	send()
+	menu.style.display = 'none'
 }
 
 function geraResposta(){
     digitado = texto.value.toLowerCase()
     let trecho = digitado.slice(0, 4)
-	if(digitado === "quem é você?" || digitado === "o que é você?" || digitado === "o que você é?"){
+    if(digitado === ''){
+        mostraResposta("não vai escrever nada não? você precisa escrever alguma coisa pra eu te responder né!")
+    }else if(digitado === "quem é você?" || digitado === "o que é você?" || digitado === "o que você é?" || digitado === "Qual o seu nome?"){
 		mostraResposta("Eu sou o chatoFDP, uma IA(Ignorância Artificial)")
 	}else if(trecho === "quem"){
         mostraResposta("Não sei quem é, não sei quem foi e nem me interessa saber!")
 	}else if(digitado === "teste" || digitado === "testando"){
         mostraResposta("Pronto, já testou!")
+	}else if(digitado === "você é burro" || digitado === "seu burro" || digitado === "burro"){
+        mostraResposta("você não sabe perguntar e eu é que sou burro!")
+	}else if(digitado === "você não entende nada"){
+        mostraResposta("você é que não sabe nada e nem sabe se expressar direito!")
 	}else if(digitado === "filho da puta"){
 		mostraResposta("você que é!")
 	}else if(trecho === "sera" || digitado === "será"){
         mostraResposta("será que existe vida fora da terra? será que o sabiá sabia assobiar? será que você é o unico que me faz pergunta idiota?")
+	}else if(digitado === "seu cu" || digitado === "seu rabo"){
+        mostraResposta("cadê sua educação? seu arrombado")
 	}else if(digitado === "viado"){
         mostraResposta("'veado' é um animal mamífero ruminante da familia cervidae. A palavra 'viado' não existe seu burro.")
 	}else if(digitado === "vai se fuder" || digitado === "vai tomar no cu" || digitado === "fodas" || digitado === "vai pra puta que pariu"){
@@ -68,12 +93,14 @@ function geraResposta(){
 		mostraResposta("cara, eu não sei nem onde eu estou, nem aonde vou, nem onde judas perdeu as botas...")
 	}else if(trecho === "fala" || trecho === "fale" || trecho === "diga"){
 		mostraResposta("não vou dizer nada, porque não sei falar, só sei escrever!")
-	}else if(trecho === "what" || trecho === "why" || trecho === "who" || trecho === "wher" || trecho === "how "){
-        mostraResposta("Eu não falo nem português direito, muito menos inglês!")
+	}else if(trecho === "what" || trecho === "why " || trecho === "who " || trecho === "wher" || trecho === "how " || trecho === "fuck"){
+        mostraResposta("Eu não entendo nem português direito, muito menos inglês!")
+	}else if(trecho === "trad"){
+        mostraResposta("não sei traduzir isso!")
 	}else if(trecho === "exis"){
         mostraResposta("existe papai noel, existe coelhinho da páscoa, fada do dente, etc. Só não existe chance de você ficar inteligente!")
 	}else if(trecho === "por " || trecho === "porq"){
-        mostraResposta("são tantos 'por ques' nesse mundo... a verdade é que eu não sei o por que!")
+        mostraResposta("são tantos 'por quês' nesse mundo... a verdade é que eu não sei o por que de nada!")
 	}else{
 		let resps = [
             "Não entendi sua pergunta, reescreva de forma mais clara, ou então peça alguem que saber escrever pra te ajudar.",
@@ -88,13 +115,12 @@ function geraResposta(){
             "Não entendi. Você escreve tão mal que nem o google vai entender!",
             "se você continuar escrevendo mal assim, não vai ter como a gente conversar!",
             "por favor, tente escrever de um jeito que eu entenda, seu analfabeto!",
-            "isso que você escreveu parace árabe, grego ou russo, sei lá! Escreva em português e de forma clara"
+            "isso que você escreveu parace árabe, grego ou russo, sei lá! Escreva em português e de forma clara",
+            "você escreveu uma coisa sem sentido! Tente novamente",
+            "é sério que você escreve assim? Escreve direito que eu não entendi"
 		]
 		let respAleatoria = Math.trunc(Math.random() * resps.length)
-		mostraResposta(
-		    resps[respAleatoria]
-		)
-		console.log(resps[respAleatoria])
+		mostraResposta(resps[respAleatoria])
 	}
 
 	chat.scrollTop = chat.scrollHeight
